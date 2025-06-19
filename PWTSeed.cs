50e0d9a4e8ec3acbce4f32f6f10a3f5cf0d6238a
namofure <213440402+namofure@users.noreply.github.com>
2025-06-19 18:52:15 +0900

    Add files via upload

--- PWTSeed.cs
+++ PWTSeed.cs
@@ -10,47 +10,54 @@ namespace PWT_RNG
     {
         private uint Seed;
         private uint Count;
+        uint[] stateVector = new uint[624];
+        protected const int N = 624;
+        protected const int M = 397;
+        protected const uint MATRIX_A = 0x9908b0df;
+        protected const uint UPPER_MASK = 0x80000000;
+        protected const uint LOWER_MASK = 0x7fffffff;
 
         public PWTSeed(uint Seed, uint Count)
         {
             this.Seed = Seed;
             this.Count = Count;
-        }
 
+            //-----------------------------------------------------------------------------------
+            stateVector[0] = Seed;
+            for (uint i = 1; i < stateVector.Length; i++)
+            {
+                stateVector[i] = 0x6C078965 * (stateVector[i - 1] ^ (stateVector[i - 1] >> 30)) + i;
+            }
+
+            for (var k = 0; k < N - M; k++)
+            {
+                var temp = (stateVector[k] & UPPER_MASK) | (stateVector[k + 1] & LOWER_MASK);
+                stateVector[k] = stateVector[k + M] ^ (temp >> 1);
+                if ((temp & 1) == 1) stateVector[k] ^= MATRIX_A;
+            }
+            for (var k = N - M; k < N - 1; k++)
+            {
+                var temp = (stateVector[k] & UPPER_MASK) | (stateVector[k + 1] & LOWER_MASK);
+                stateVector[k] = stateVector[k + (M - N)] ^ (temp >> 1);
+                if ((temp & 1) == 1) stateVector[k] ^= MATRIX_A;
+            }
+            {
+                var temp = (stateVector[N - 1] & UPPER_MASK) | (stateVector[0] & LOWER_MASK);
+                stateVector[N - 1] = stateVector[M - 1] ^ (temp >> 1);
+                if ((temp & 1) == 1) stateVector[N - 1] ^= MATRIX_A;
+            }
+            //--------------------------------------------------------------------------------------
+        }
         public ulong PWTRNG()
         {
-            uint value1 = 0x0;
-            uint value2 = 0x0;
-            uint value3 = 0x0;
-            uint Const1 = 0x0;
-            uint Const2 = 0x0;
-            uint Const3 = 0x0;
-            uint MT1, MT2, MT3, MT4;
+
             uint val1, val2, val3;
             uint temp1, temp2, temp3, temp4, temp5, temp6;
             ulong PWT, PWT1, PWT2, PWTtemp;
             uint temp = Seed;
 
-            for (uint i = 1; i <= 624; i++) //個体値乱数の生成
-            {
-                temp = ((temp ^ (temp >> 30)) * 0x6C078965) + i;
-                if (i == Count + 1) value1 = temp;
-                if (i == Count + 2) value2 = temp;
-                if (i == Count + 3) value3 = temp;
-                if (i == Count + 398) Const1 = temp;
-                if (i == Count + 399) Const2 = temp;
-
-            }
-
-            MT1 = (value1 & 0x80000000) | (value2 & 0x7FFFFFFF);    //テーブル更新
-            MT2 = (MT1 >> 0x1) ^ Const1;
-            if (MT1 % 2 == 1) MT2 = MT2 ^ 0x9908B0DF;
-            val1 = MT2;
-
-            MT3 = (value2 & 0x80000000) | (value3 & 0x7FFFFFFF);
-            MT4 = (MT3 >> 0x1) ^ Const2;
-            if (MT3 % 2 == 1) MT4 = MT4 ^ 0x9908B0DF;
-            val2 = MT4;
+            val1 = stateVector[Count];
+            val2 = stateVector[Count + 1];
 
             temp1 = (val1 >> 0xB) ^ val1;
             temp2 = ((temp1 << 0x7) & 0x9D2C5680) ^ temp1;
--- TournamentSearcher.cs
+++ TournamentSearcher.cs
@@ -7,89 +7,223 @@ using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PWT_test
+namespace PWT_RNG
 {
     internal class TournamentSearcher
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("======================================");
+            Console.Write("PWT Tournament Searcher\n");
+            Console.Write("Initial SEED : 0x");   // 初期SEED入力
 
+            if (uint.TryParse(Console.ReadLine(), System.Globalization.NumberStyles.HexNumber, null, out uint Seed))
             {
-                Console.WriteLine("======================================");
-                Console.Write("PWT RNG Tool\n");
-                Console.Write("Initial SEED : 0x");   // 初期SEED入力
+                string outputPath = $"TournamentList.txt";
+                string excludePath = "出禁リスト.txt";
 
-                    if (uint.TryParse(Console.ReadLine(), System.Globalization.NumberStyles.HexNumber, null, out uint Seed))
+                List<string> ExcludeList = new();
+                if (File.Exists(excludePath))
+                {
+                    ExcludeList = File.ReadAllLines(excludePath)
+                        .Select(line => line.Trim())
+                        .Where(line => !string.IsNullOrWhiteSpace(line))
+                        .ToList();
+                }
+                else
+                {
+                    File.WriteAllText(excludePath, "");
+                }
+
+                    using (StreamWriter writer = new(outputPath))
                     {
-                        Console.Write("Entry Count：");  //受付回数入力
-                        if (uint.TryParse(Console.ReadLine(), out uint count) && count > 0)
+                        writer.WriteLine("【World Leaders Tournament】");
+                        writer.WriteLine($"Initial Seed：0x{Seed:X8}");
+                        writer.WriteLine("");
+
+                        for (uint count = 1; count < 623; count++)
                         {
-                            string outputPath = $"PWT RNG.txt";
-                            using (StreamWriter writer = new(outputPath))
+                            var PWT = new PWTSeed(Seed, count);
+                            ulong PWTSeed = PWT.PWTRNG();
+
+                            List<ulong> TrainerIDs = [8, 14, 15, 16, 17, 18, 19, 53];
+                            List<ulong> JoinIDs = new List<ulong>();
+
+                            ulong JoinIndex = 0x2E;
+
+                            ulong temp = NextSeed(PWTSeed);
+
+                            for (int i = 0; i < 7; i++, temp = NextSeed(temp))
                             {
+                                ulong TrainerIndex = 0;
+                                ulong RawTrainerIndex = ((temp >> 32) * JoinIndex) >> 32;
 
-                                var PWT = new PWTSeed(Seed, count);
-                                ulong PWTSeed = PWT.PWTRNG();
-                                Console.WriteLine($"PWTSeed：0x{PWTSeed:X16}");
-
-                                List<ulong> TrainerIDs = [8, 14, 15, 16, 17, 18, 19, 53];
-                                List<ulong> JoinIDs = new List<ulong>();
-
-                                ulong JoinIndex = 0x2E;
-
-                                ulong temp = NextSeed(PWTSeed);
-
-                                for (int i = 0; i < 7; i++, temp = NextSeed(temp))
+                                while (RawTrainerIndex > 0)
                                 {
-                                    ulong TrainerIndex = 0;
-                                    ulong RawTrainerIndex = ((temp >> 32) * JoinIndex) >> 32;
-
-                                    while (RawTrainerIndex > 0)
-                                    {
-                                        TrainerIndex++;
-
-                                        if (TrainerIDs.Contains(TrainerIndex))
-                                        {
-                                            continue;
-                                        }
-
-                                        RawTrainerIndex--;
-                                    }
+                                    TrainerIndex++;
 
                                     if (TrainerIDs.Contains(TrainerIndex))
                                     {
-                                        TrainerIndex++;   //例外処理？
+                                        continue;
                                     }
 
-                                    TrainerIDs.Add(TrainerIndex);
-                                    JoinIDs.Add(TrainerIndex);
-                                    JoinIndex--;
+                                    RawTrainerIndex--;
                                 }
 
-                                string[] TrainerName = {"チェレン","ホミカ","アーティ","カミツレ","ヤーコン","フウロ","ジャガ","シズイ","ベル","ポッド",
-                                    "コーン","デント","アロエ","ハチク","グリーン","ワタル","ダイゴ","ミクリ","シロナ","アデク",
-                                    "タケシ","カスミ","マチス","エリカ","ナツメ","カツラ","サカキ","ハヤト","ツクシ","アカネ",
-                                    "マツバ","シジマ","ミカン","ヤナギ","イブキ","アンズ","ツツジ","トウキ","テッセン","アスナ",
-                                    "センリ","ナギ","フウ","ラン","アダン","ヒョウタ","ナタネ","メリッサ","スモモ","マキシ",
-                                    "トウガン","スズナ","デンジ","レッド",};
-
-                                List<string> JoinedTrainer = JoinIDs.ConvertAll(id => TrainerName[(ulong)id]);
-
-                                Console.WriteLine("参加トレーナー");
-
-                                for (int i = 0; i < JoinedTrainer.Count; i++)
+                                if (TrainerIDs.Contains(TrainerIndex))
                                 {
-                                    Console.WriteLine($"{i + 1}.{JoinedTrainer[i]}");
+                                    TrainerIndex++;   //例外処理？
                                 }
 
-                                Console.WriteLine($"PWTSeed：0x{PWTSeed:X16}");
-
-                                Console.WriteLine("======================================");
+                                TrainerIDs.Add(TrainerIndex);
+                                JoinIDs.Add(TrainerIndex);
+                                JoinIndex--;
                             }
+
+                            JoinIDs.Sort();
+
+                            string[] TrainerName = {"チェレン","ホミカ","アーティ","カミツレ","ヤーコン","フウロ","ジャガ","シズイ","ベル","ポッド",
+                            "コーン","デント","アロエ","ハチク","グリーン","ワタル","ダイゴ","ミクリ","シロナ","アデク",
+                            "タケシ","カスミ","マチス","エリカ","ナツメ","カツラ","サカキ","ハヤト","ツクシ","アカネ",
+                            "マツバ","シジマ","ミカン","ヤナギ","イブキ","アンズ","ツツジ","トウキ","テッセン","アスナ",
+                            "センリ","ナギ","フウ","ラン","アダン","ヒョウタ","ナタネ","メリッサ","スモモ","マキシ",
+                            "トウガン","スズナ","デンジ","レッド","YOU"};
+
+                            List<string> JoinedTrainer = JoinIDs.ConvertAll(id => TrainerName[(ulong)id]);
+                            JoinedTrainer.Insert(0, "YOU");
+                            JoinIDs.Insert(0, 0xFF);
+
+                            temp = NextSeed(PWTSeed);
+
+                            for (ulong i = 0; i < 8; i++, temp = NextSeed(temp))
+                            {
+                                ulong SwapIndex = ((temp >> 32) * (8 - i)) >> 32;
+
+                                int Swap = (int)SwapIndex;
+                                int I = (int)(7 - i);
+
+                                ulong tempID = JoinIDs[Swap];
+                                JoinIDs[Swap] = JoinIDs[I];
+                                JoinIDs[I] = tempID;
+
+                                string tempName = JoinedTrainer[Swap];
+                                JoinedTrainer[Swap] = JoinedTrainer[I];
+                                JoinedTrainer[I] = tempName;
+
+                            }
+
+                            byte[] A = { 05, 05, 05, 05, 01, 01, 01, 01 };
+                            byte[] B = { 03, 03, 01, 01, 07, 07, 05, 05 };
+                            byte[] C = { 01, 00, 03, 02, 05, 04, 07, 06 };
+
+                            int YourIndex = JoinedTrainer.IndexOf("YOU");
+                            int AIndex = A[YourIndex];
+                            int BIndex = B[YourIndex];
+                            int CIndex = C[YourIndex];
+
+                            var WithoutYou = JoinedTrainer
+                                .Where(name => name != "YOU")
+                                .ToList();
+
+                            temp = NextSeed(temp);
+                            temp = NextSeed(temp);
+
+                            for (ulong i = 0; i < 4; i++, temp = NextSeed(temp))
+                            {
+                                ulong SwapIndex = ((temp >> 32) * (4 - i)) >> 32;
+
+                                int Swap = (int)(SwapIndex + 3);
+                                int I = (int)(6 - i);
+                                string tempName = WithoutYou[Swap];
+                                WithoutYou[Swap] = WithoutYou[I];
+                                WithoutYou[I] = tempName;
+
+                            }
+
+                            string[] WorldLeaders = new string[8];
+
+                            WorldLeaders[YourIndex] = JoinedTrainer[YourIndex];
+                            WorldLeaders[AIndex] = WithoutYou[0];
+                            WorldLeaders[BIndex] = WithoutYou[1];
+                            WorldLeaders[CIndex] = WithoutYou[2];
+
+                            int Count = 3;
+                            for (int i = 0; i < 8; i++)
+                            {
+                                if (WorldLeaders[i] == null)
+                                {
+                                    WorldLeaders[i] = WithoutYou[Count];
+                                    Count++;
+                                }
+                            }
+
+                            //-----------------------------------------
+                            uint maxY = (count - 1) / 7;
+                            uint x = 0;
+                            uint y = 0;
+                            bool found = false;
+
+                            for (uint yTry = maxY; yTry <= maxY; yTry--)
+                            {
+                                uint remaining = (count - 1) - 7 * yTry;
+                                if (remaining % 2 == 0)
+                                {
+                                    x = remaining / 2;
+                                    y = yTry;
+                                    found = true;
+                                    break;
+                                }
+                                if (yTry == 0) break;
+                            }
+                            //-----------------------------------------
+
+                            bool hasNoExcluded = JoinedTrainer.All(name => !ExcludeList.Contains(name));
+
+
+
+                            string PadDisplay(string s, int padWidth)
+                            {
+                                int width = s.Sum(c => (c <= 0x7F ? 1 : 2));
+                                return s + new string(' ', Math.Max(0, padWidth - width));
+                            }
+
+                            if (hasNoExcluded)
+                            {
+                                Console.WriteLine("");
+                                if (found) { Console.WriteLine($"Offset：{(count - 1)}, 受付：{x}, ボックス：{y}"); }
+                                else { Console.WriteLine($"Offset：{(count - 1)}"); }
+                                Console.WriteLine($"PWTRNG：0x{PWTSeed:X16}");
+
+                                for (int i = 0; i < 4; i++)
+                                {
+                                    string half1 = PadDisplay(WorldLeaders[i], 10);
+                                    string half2 = PadDisplay(WorldLeaders[i + 4], 10);
+
+                                    Console.WriteLine($"    {half1}{half2}");
+                                }
+                            }
+
+                            if (found) { writer.WriteLine($"Offset：{(count - 1)}, 受付：{x}, ボックス：{y}"); }
+                            else { writer.WriteLine($"Offset：{(count - 1)}"); }
+                            writer.WriteLine($"PWTRNG：0x{PWTSeed:X16}");
+
+                            for (int i = 0; i < 4; i++)
+                            {
+                                string half1 = PadDisplay(WorldLeaders[i], 10);
+                                string half2 = PadDisplay(WorldLeaders[i + 4], 10);
+
+                                writer.WriteLine($"    {half1}{half2}");
+                            }
+
+                            writer.WriteLine("");
                         }
-                    }
-            }
+                        Console.WriteLine("======================================");
+                        Console.ReadKey();
+                    } 
+                }
+                    
         }
+        
         static ulong NextSeed(ulong PWTSeed)
         {
             ulong a = 0x5D588B656C078965;
